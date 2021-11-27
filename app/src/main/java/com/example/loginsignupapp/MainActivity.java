8c6efa5c3a7e607a6cc9f5265fd9c5f4b3bd1a6b
Aseel Abdallah <aseelabd512004@gmail.com>
2021-11-27 10:35:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/loginsignupapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.loginsignupapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etUserName, etPasword;
+    Button btnIogIn,btnSighnup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etPasword = findViewById(R.id.etpasswordMain);
+        etUserName = findViewById(R.id.etUserNameMain);
+    }
+public void login(View view)
+{
+    String username=etUserName.getText().toString();
+    String password = etPasword.getText().toString();
+    if(username.trim().isEmpty()|| password().trim().isEmpty())
+    {
+        Toast.makeText(this, "Username or password is missing", Toast.LENGTH_SHORT).show();
+        return;
+
+}
+}
+
+
+    public void Login(View view) {
+    }}
